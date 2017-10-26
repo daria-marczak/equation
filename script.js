568fb6c1dd381231bf1ea11dc3906eebd41cf46d
daria marczak <marczak.ria@gmail.com>
2017-10-26 20:40:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var a = 0;
+var b = 0;
+
+var value = (a * a) - (2 * a * b) + (b * b);
+
+if (value > 0) {
+	alert("Bigger than zero");
+}
+else if (value < 0) {
+	alert("Smaller than zero");
+}
+else {
+	alert("Equals to zero");
+}
